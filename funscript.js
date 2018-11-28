681a31a639672eb1f361c6681e45bfffd13f7510
AlexnderLusth <alexander0906@live.se>
2018-11-28 11:53:19 +0100

    more text to alert

--- funscript.js
+++ funscript.js
@@ -1,1 +1,1 @@
-alert ("audi rs7. expensive right? it is really fast right? click OK to se fun fact about cars right?");
+alert ("audi rs7. expensive right? it is really fast right? click OK to se fun fact about cars right? or other engines or compuer stuff or anythung right?");
