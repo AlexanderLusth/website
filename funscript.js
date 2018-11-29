9410af271ea2adbc5c919eed4aabfcb7930cb0cd
AlexnderLusth <alexander0906@live.se>
2018-11-29 10:48:39 +0100

    added pictures. version 1.1

--- funscript.js
+++ funscript.js
@@ -1,1 +1,1 @@
-alert ("audi rs7. expensive right? it is really fast right? click OK to se fun fact about cars right? or other engines or compuer stuff or anything right?");
+alert ("audi rs7. expensive right? click ok for info and other autism stuff right?");
