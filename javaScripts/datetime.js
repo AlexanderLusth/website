2188b76ed3bc43aea89f7b77448ce598930acf77
AlexnderLusth <alexander0906@live.se>
2018-12-04 08:13:02 +0100

    added comments

--- javaScripts/datetime.js
+++ javaScripts/datetime.js
@@ -1,5 +1,5 @@
 function getDate(){
-//deklarerar en variabel
+//deklarerar en variabel "function"
 var datum = new Date();
 //hämtar och sätter id när kommandot ges
 document.getElementById("datumet").innerHTML=datum
--- javaScripts/funscript.js
+++ javaScripts/funscript.js
@@ -1,1 +1,2 @@
+//gör en alert på sidan om audi rs7
 alert ("audi rs7. expensive right? click ok for info and other autism stuff right?");
