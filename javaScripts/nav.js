02f3ab758104a127e106ac04dd5ef68ba23f3c3d
AlexnderLusth <alexander0906@live.se>
2019-02-11 09:36:23 +0100

    removed autism

--- javaScripts/nav.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("navmain");
-    if (x.className.indexOf("nav-ul") == -1) {
-        x.className += " nav-ul";
-    } else {
-        x.className = x.className.replace(" w3-show", "");
-    }
-}
